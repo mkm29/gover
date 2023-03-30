ec4d42496d6a703d7503c0ebd0bd374aff9517e5
mitchell.murphy <mitchell.murphy@sclzg2.onmicrosoft.us>
2023-03-30 18:33:34 -0400

    Merged in testing and update-viper branches; moved Dockerfiles to docker directory; updated build.sh (added labels)

--- build.sh
+++ build.sh
@@ -1,18 +1,37 @@
 #!/bin/sh
 
 OS=linux # `uname -s | tr A-Z a-z`
-ARCH=amd64 # `uname -m`
-DOCKERFILE=Dockerfile
-VERSION=0.1.2-development-alpha.3
+ARCH=arm64 # `uname -m`
+DOCKERFILE=docker/Dockerfile
+VERSION=$(./bin/gover version | sed 's/\+/-build./g')
+
+echo Logging into Nexus
+echo $NEXUS_PASS | docker login -u $NEXUS_USER --password-stdin $NEXUS_URL
+
+IMG="$NEXUS_URL/ssf-tools/gover:$VERSION"
+echo "Building $IMG"
 
 if [ $ARCH == "amd64" ]; then
-    docker build -t nexus.ssf.sclzdev.net/ssf-tools/gover:$VERSION \
+    docker build -t $IMG \
         --build-arg=TARGETOS=$OS --build-arg=TARGETARCH=$ARCH \
-        -f $DOCKERFILE .
-    docker push nexus.ssf.sclzdev.net/ssf-tools/gover:$VERSION
+        -f $DOCKERFILE \
+        --label GIT_COMMIT=$(git rev-parse HEAD) \
+        --label GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD) \
+        --label MAINTAINER="Mitchell Murphy<mitchell.k.murphy.ctr@socom.mil>" \
+        --label VERSION=$VERSION \
+        --label OS=$OS \
+        --label ARCH=$ARCH \
+        .
+    docker push $NEXUS_URL/ssf-tools/gover:$VERSION
 else
     docker buildx build --platform "${OS}/${ARCH}" \
         --build-arg=TARGETOS=$OS --build-arg=TARGETARCH=$ARCH \
         -f $DOCKERFILE --push \
-        -t "nexus.ssf.sclzdev.net/ssf-tools/gover:$VERSION" .
+        --label GIT_COMMIT=$(git rev-parse HEAD) \
+        --label GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD) \
+        --label MAINTAINER="Mitchell Murphy<mitchell.k.murphy.ctr@socom.mil>" \
+        --label VERSION=$VERSION \
+        --label OS=$OS \
+        --label ARCH=$ARCH \
+        -t $IMG .
 fi
